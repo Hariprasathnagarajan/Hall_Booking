1172e5d829404071531248d8f020bbbd580f745d
Hariprasathnagarajan <hariprasathnagarajan24@gmail.com>
2025-08-19 17:23:12 +0530

    created hall master

--- backend/hall_api/models.py
+++ backend/hall_api/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+class hall_Master(models.Model):
+    Off_Code = models.CharField(max_length=10)
+    Hall_Code = models.CharField( max_length=20)
+    Hall_Name = models.CharField(max_length=50)
+    DAY_SPOC_Name = models.CharField( max_length=20)
+    NGT_SPOC_Name = models.CharField( max_length=20)
+    Capacity = models.IntegerField()
+    Table = models.IntegerField()
+    Table = models.IntegerField()
+    Chairs = models.IntegerField()
+    White_Board = models.BooleanField()
+    Video = models.BooleanField()
+    Audio = models.BooleanField()
+    Wifi = models.BooleanField()
+    Others = models.CharField()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
