56180449f6a50dbf2cdc3ea1855985a24c07dabd
Hariprasathnagarajan <hariprasathnagarajan24@gmail.com>
2025-08-20 09:43:49 +0530

    removed hall master table

--- backend/hall_api/models.py
+++ backend/hall_api/models.py
@@ -66,28 +66,6 @@ class Booking(models.Model):
     def __str__(self):
         return f"{self.emp_name} - {self.hall_code} on {self.book_date}"
 
-class hall_Master(models.Model):
-    Off_Code = models.CharField(max_length=10)
-    Hall_Code = models.CharField( max_length=20)
-    Hall_Name = models.CharField(max_length=50)
-    DAY_SPOC_Name = models.CharField( max_length=20)
-    NGT_SPOC_Name = models.CharField( max_length=20)
-    Capacity = models.IntegerField()
-    Table = models.IntegerField()
-    Table = models.IntegerField()
-    Chairs = models.IntegerField()
-    White_Board = models.BooleanField()
-    Video = models.BooleanField()
-    Audio = models.BooleanField()
-    Wifi = models.BooleanField()
-    Others = models.CharField()
-
-
-
-
-
-
-
 
 
 
